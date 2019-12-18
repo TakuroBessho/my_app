dd5311eff38c5566e64cc8fed099d79eb7b7eb22
TakuroBessho <58577692+TakuroBessho@users.noreply.github.com>
2019-12-18 12:12:33 +0900

    Update common.py

--- api/common.py
+++ api/common.py
@@ -5,10 +5,10 @@ from email.mime.text import MIMEText
 def send_mail(msg):
     SMTP_SERVER = "smtp.mail.yahoo.co.jp"
     SMTP_PORT = 587
-    SMTP_USERNAME = "oruka2365@yahoo.co.jp"  # 送信元アドレス
-    SMTP_PASSWORD = "Bessho199665@"  # パスワード
-    EMAIL_FROM = "oruka2365@yahoo.co.jp"  # 送信元アドレス
-    EMAIL_TO = "oruka199665@yahoo.co.jp"  # 送信先アドレス
+    SMTP_USERNAME = "o"  # 送信元アドレス
+    SMTP_PASSWORD = ""  # パスワード
+    EMAIL_FROM = "p"  # 送信元アドレス
+    EMAIL_TO = ""  # 送信先アドレス
     EMAIL_SUBJECT = "ビットフライヤーの自動取引プログラムの運用報告について"
     co_msg = msg
     msg = MIMEText(co_msg)
